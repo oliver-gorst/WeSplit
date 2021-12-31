1140dc23a961e10d02ea5b4cea8027a2c5edd5af
Oliver Gorst <oliver.gorst@outlook.com>
2021-12-31 16:25:26 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Oliver Gorst on 12/31/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
