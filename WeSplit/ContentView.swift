3384e0eb11e49dee523727549de0cff33f9c52e3
Oliver Gorst <oliver.gorst@outlook.com>
2022-01-03 13:15:32 -0500

    added picker, added segmented control, added header to section

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -15,16 +15,38 @@ struct ContentView: View {
     let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
+        NavigationView {
         Form {
             Section {
                 TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
                     .keyboardType(.decimalPad)
+                Picker("Number of people", selection: $numberOfPeople) {
+                    ForEach(2..<100) {
+                        Text("\($0) people")
+                    }
+                }
             }
+            
+            Section{
+                Picker("Tip percentage", selection: $tipPercentage) {
+                    ForEach(tipPercentages, id: \.self) {
+                        Text($0, format: .percent)
+                        
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+        header:{
+            Text("How much tip do you want to leave?")
+        }
+            
             Section {
                 Text(checkAmount, format: .currency(code:Locale.current.currencyCode ?? "USD"))
             }
         }
+        .navigationTitle("WeSplit")
 }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
